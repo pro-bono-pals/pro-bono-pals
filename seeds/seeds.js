265e7c7727d829b581410ba608d23c5fe106e0b3
amadeamargo <amadeamargo@gmail.com>
2022-01-15 12:16:50 -0600

    added task routes

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,10 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, Profile, Education, Service } =  require('../models')
+const { User, Profile, Education, Service, Task } =  require('../models')
 
 const userData = require('./userData.json');
 const educationData = require('./educationData.json');
 const serviceData = require('./serviceData.json');
 const profileData = require('./profileData.json');
+const taskData = require('./taskData.json');
 
 
 const seedDatabase = async () => {
@@ -17,6 +18,7 @@ const seedDatabase = async () => {
     const educations = await Education.bulkCreate(educationData)
     const services = await Service.bulkCreate(serviceData)
     const profiles = await Profile.bulkCreate(profileData)
+    const task = await Task.bulkCreate(taskData)
 
 
       process.exit(0);
