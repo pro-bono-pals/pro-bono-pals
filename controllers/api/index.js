167f47727001a20924ea99e1d2f2548984168fb4
FionaN <fiona.nicdao@northwestern.edu>
2022-01-15 11:05:16 -0600

    Profileroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // const educationRoutes = require('./educationRoutes');
-// const profileRoutes = require('./profileRoutes');
+const profileRoutes = require('./profileRoutes');
 // const serviceRoutes = require('./serviceRoutes');
 // const taskRoutes = require('./taskRoutes');
 const userRoutes = require('./userRoutes');
@@ -10,7 +10,7 @@ const userRoutes = require('./userRoutes');
 
 
 // router.use('/education', educationRoutes);
-// router.use('/profile', profileRoutes);
+router.use('/profile', profileRoutes);
 // router.use('/service', serviceRoutes);
 // router.use('/task', taskRoutes);
 router.use('/user', userRoutes);
