dd81b131d50bfacd4b3f611dd2f5588def4cf199
FionaN <fiona.nicdao@northwestern.edu>
2022-01-17 19:33:59 -0600

    handelbars

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const sess = {
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
