46b2cd014010891bcd608c8188523605b1d3ef6e
amadeamargo <amadeamargo@gmail.com>
2022-01-17 20:06:21 -0600

    added login page and updated handlebars css

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+const { response } = require("express");
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/user/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json'},
+    });
+
+            if (response.ok) {
+                document.location.replace('/profile');
+            } else {
+                alert(response.statusText);
+            }
+        }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const isProvider = document.querySelector('#is-provider-signup').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headres: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                document.location.replace('/profile');
+            } else {
+                alert(response.statusText);
+            }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document 
+    .querySeled ('.signup-form')
+    .addEventListener('submit', signupFormHandler)
