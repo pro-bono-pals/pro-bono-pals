c00fba9367b963358bb8e4170822642d2fa389ce
amadeamargo <amadeamargo@gmail.com>
2022-01-19 18:56:44 -0600

    updated css and handelbars

---
+++ public/js/logout.js
@@ -0,0 +1,20 @@
+const logoutBtn= document.querySelector('#logout-btn');
+
+const logout = async () => {
+
+    console.log("you have signed out")
+ //note: will need to make a route to sign out
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+
+
+logoutBtn.addEventListener('click', logout)
