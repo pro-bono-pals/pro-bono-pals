8e2c693a9f7389437450be8fe09b80e04b989a19
FionaN <fiona.nicdao@northwestern.edu>
2022-01-22 08:02:36 -0600

    functional signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+// const { response } = require("express");
 
 const signupFormHandler = async (event) => {
     event.preventDefault();
@@ -6,10 +6,46 @@ const signupFormHandler = async (event) => {
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const isProvider = document.querySelector('#is-provider-signup').value.trim();
-    const zipcode = document.querySelector('#zipcde-signup').value.trim;
-    const disclaimer = document.querySelector('#disclaimer-signup').value.trim();
+
+    const provider = document.querySelector('#is-provider-signup')
+    const isProvider = provider.checked;
+    console.log(isProvider);
+
+    let zipcode = document.querySelector('#zipcde-signup')
+
+
+    const claimer = document.querySelector('#disclaimer-signup')
+    const disclaimer = claimer.checked;
+    console.log(disclaimer);
+
     const education = document.querySelector('#education-signup').value.trim();
+    let educationId
+    const geteducationId = function() {
+      if(education === "high-school"){
+        educationId = 6
+        return;
+      }else if (education ==="some-college"){
+        educationId = 7
+        return;
+      }else if(education === "professional-school"){
+        educationId = 8
+      }else if (education ==="associate"){
+        educationId = 9
+        return;
+      }else if(education ==="bachelors"){
+        educationId = 2
+        return;
+      }else if(education ==="masters"){
+        educationId = 3
+        return;
+      }else if(education ==="doctorate"){
+        educationId = 4
+        return;
+      }else{
+        educationId = 10
+      }
+    }
+    geteducationId()
 
     toastr.options = {
         "closeButton": false,
@@ -28,17 +64,18 @@ const signupFormHandler = async (event) => {
         "showMethod": "fadeIn",
         "hideMethod": "fadeOut"
       }
+    console.log(name, email, password,isProvider, zipcode, disclaimer, educationId)
 
-
-    if (name && email && password) {
-        const response = await fetch('api/users', {
+    if (name && email && password && isProvider && disclaimer && educationId) {
+        const response = await fetch('api/user/', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ name, email, password, isProvider, disclaimer,educationId }),
             headres: { 'Content-Type': 'application/json' },
             });
-
+            console.log("hahhaa")
             if (response.ok) {
-                document.location.replace('/profile');
+                document.location.replace('/dashboard');
+                console.log("hello", response)
             } else {
                 toastr.success("You have successfully signed up to Pro Bono Pals!", "Success!")
             }
