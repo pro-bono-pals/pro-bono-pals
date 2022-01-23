c3d0406e8039f3a74985f4b6ea9ba410fdd410cb
FionaN <fiona.nicdao@northwestern.edu>
2022-01-23 17:33:20 -0600

    fix signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,20 +1,16 @@
 const router = require('express').Router();
-const isAuth = require('../../utils/auth')
 const { User } = require('../../models');
 
-router.post('/',isAuth ,async (req, res) => {
+router.post('/' ,async (req, res) => {
   try {
-    const userData = await User.create({
-      ...req.body,
-      user_id:req.session.user_id,
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.logged_in = true;
     });
 
-    // req.session.save(() => {
-    //   req.session.userId = userData.id;
-    //   req.session.logged_in = true;
-
-      res.status(200).json(userData, { message: 'User created!'});
-  
+    res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
   }
